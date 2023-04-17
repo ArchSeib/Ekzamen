cb2b036051b8075f35ec7e14b243359ef993dacd
ArchSeib <ruslan.bakybaev@mail.ru>
2023-04-17 09:47:37 +0700

    Добавьте файлы проекта.

---
+++ Ekzamen/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+
+
+namespace Ekzamen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Regex regYear = new Regex(@"^\d{4}$");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static bool CheckEmpty(String value)
+        {
+            if (value == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool CheckYearEmpty(String value)
+        {
+            if (value == "____")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool CheckAuthor(String value)
+        {
+            if (value.Length < 20)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool CheckName(String value)
+        {
+            if (value.Length < 10)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool CheckYear(String value)
+        {
+            if (regYear.IsMatch(value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private bool ExpectedTrue = true;
+        private bool ExpectedFalse = false;
+        [TestMethod]
+        public void TestMethod11()
+        {
+            String check = "";
+            var result = Ekzamen.MainWindow.CheckEmpty(check);
+            Assert.AreEqual(ExpectedFalse, result);
+        }
+        [TestMethod]
+        public void TestMethod12()
+        {
+            String check = "erewrrwe ewrwerwe wer";
+            var result = Ekzamen.MainWindow.CheckEmpty(check);
+            Assert.AreEqual(ExpectedTrue, result);
+        }
+        [TestMethod]
+        public void TestMethod21()
+        {
+            String check = "123456789";
+            var result = Ekzamen.MainWindow.CheckAuthor(check);
+            Assert.AreEqual(ExpectedFalse, result);
+        }
+        [TestMethod]
+        public void TestMethod22()
+        {
+            String check = "1234567890123456789012";
+            var result = Ekzamen.MainWindow.CheckAuthor(check);
+            Assert.AreEqual(ExpectedTrue, result);
+        }
+        [TestMethod]
+        public void TestMethod31()
+        {
+            String check = "12452532";
+            var result = Ekzamen.MainWindow.CheckName(check);
+            Assert.AreEqual(ExpectedFalse, result);
+        }
+        public void TestMethod32()
+        {
+            String check = "12345678901";
+            var result = Ekzamen.MainWindow.CheckName(check);
+            Assert.AreEqual(ExpectedTrue, result);
+        }
+        [TestMethod]
+        public void TestMethod41()
+        {
+            String check = "____";
+            var result = Ekzamen.MainWindow.CheckYearEmpty(check);
+            Assert.AreEqual(ExpectedFalse, result);
+        }
+        [TestMethod]
+        public void TestMethod42()
+        {
+            String check = "_334";
+            var result = Ekzamen.MainWindow.CheckYearEmpty(check);
+            Assert.AreEqual(ExpectedTrue, result);
+        }
+        [TestMethod]
+        public void TestMethod51()
+        {
+            String check = "232";
+            var result = Ekzamen.MainWindow.CheckYear(check);
+            Assert.AreEqual(ExpectedFalse, result);
+        }
+        [TestMethod]
+        public void TestMethod52()
+        {
+            String check = "2001";
+            var result = Ekzamen.MainWindow.CheckYear(check);
+            Assert.AreEqual(ExpectedTrue, result);
+        }
+    }
+}
